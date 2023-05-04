0041dcb1d4704a9ad314008309f97c74fcd3a9c0
alvinpanerio <panerio.alvin18@gmail.com>
2023-05-04 23:36:19 +0800

    Responsive Leaderboards

--- frontend/src/pages/Leaderboards.jsx
+++ frontend/src/pages/Leaderboards.jsx
@@ -224,24 +224,28 @@ function Leaderboards() {
               Leaderboards
             </p>
           </div>
-          <div className="mt-10 w-full flex gap-3">
+          <div className="2xl:mt-10 mt-5 w-full flex gap-3">
             <div
               ref={divRef}
-              className="flex justify-between w-2/3 bg-white p-5 rounded-lg shadow-md"
+              className="flex justify-between w-2/3 bg-white p-5 rounded-lg shadow-md h-max"
             >
               <div className="flex gap-5">
-                <div className="relative w-[150px] h-[150px]">
+                <div className="relative 2xl:w-[150px] 2xl:h-[150px] w-[120px] h-[120px]">
                   <img src={border} alt="" className="absolute w-[150px]" />
                   <img
                     src={pic}
                     alt=""
-                    className="w-[70px] h-[70px] absolute right-10 top-10"
+                    className="2xl:w-[70px] 2xl:h-[70px] w-[60px] h-[60px] absolute 2xl:right-10 2xl:top-10 right-[1.875rem] top-[1.875rem]"
                   />
                 </div>
                 <div className="flex flex-col justify-between">
                   <div>
-                    <p className="text-blue-500 text-2xl font-bold">{name}</p>
-                    <p className="font-medium text-gray-500">{uid}</p>
+                    <p className="text-blue-500 2xl:text-2xl text-xl font-bold">
+                      {name}
+                    </p>
+                    <p className="font-medium text-gray-500 2xl:text-base text-sm">
+                      {uid}
+                    </p>
                   </div>
                   <div className="flex gap-3">
                     <p className="font-bold text-lg text-blue-500 w-max">
@@ -266,18 +270,20 @@ function Leaderboards() {
                   </div>
                 </div>
               </div>
-              <button className="rounded-lg bg-blue-500 shadow-lg py-3 px-5 text-white flex gap-3 items-center h-max">
+              {/* <button className="rounded-lg bg-blue-500 shadow-lg py-3 px-5 text-white flex gap-3 items-center h-max">
                 <FaShare className="text-white" />
                 Share
-              </button>
+              </button> */}
             </div>
-            <div className="gap-5 w-1/3 bg-white p-5 rounded-lg shadow-md flex flex-col justify-between">
-              <p className="text-blue-500 font-bold text-2xl">
+            <div className="2xl:gap-5 gap-4 w-1/3 bg-white p-5 rounded-lg shadow-md flex flex-col justify-between">
+              <p className="text-blue-500 font-bold 2xl:text-2xl text-xl">
                 Monthly Reset Countdown:
               </p>
               <div>
-                <p className="text-red-500 text-2xl font-semibold">{date}</p>
-                <p className="text-red-500 text-lg font-semibold">
+                <p className="text-red-500 2xl:text-2xl text-xl font-semibold">
+                  {date}
+                </p>
+                <p className="text-red-500 2xl:text-lg text-base font-semibold">
                   {countdown} Remaining
                 </p>
               </div>
@@ -285,17 +291,17 @@ function Leaderboards() {
           </div>
           <div className="w-full bg-white rounded-lg shadow-md mt-3 px-10 py-5">
             <div className="flex justify-between">
-              <p className="text-blue-500 text-2xl font-bold mb-7">
+              <p className="text-blue-500 text-xl 2xl:text-2xl font-bold 2xl:mb-7 mb-5">
                 My Friends
               </p>
               <button
-                className="rounded-lg bg-blue-500 shadow-lg py-3 px-5 text-white flex gap-3 items-center h-max"
+                className="rounded-lg bg-blue-500 shadow-lg 2xl:py-3 2xl:px-5 py-2 px-4 text-white flex gap-3 items-center h-max"
                 onClick={capture}
               >
                 <FaCamera />
               </button>
             </div>
-            <div className="flex flex-col gap-3 max-h-[328px] overflow-auto">
+            <div className="flex flex-col gap-3 2xl:max-h-[328px] max-h-[208px] overflow-auto">
               {friendsInLb
                 ?.sort((a, b) => b?.points - a?.points)
                 ?.map((i, z) => {
@@ -305,28 +311,30 @@ function Leaderboards() {
                         <img
                           src="https://cdn3d.iconscout.com/3d/premium/thumb/trophy-number-one-8616537-6815626.png"
                           alt=""
-                          className="h-[70px]"
+                          className="2xl:h-[70px] h-[50px]"
                         />
-                        <div className="rounded-full bg-blue-100 w-2/5 flex items-center justify-between pr-10">
+                        <div className="rounded-full bg-blue-100 w-[485px] flex items-center justify-between pr-10">
                           <div className="flex gap-5 items-center">
                             <img
                               src={i?.profileAvatar}
-                              className="w-[70px] h-[70px]"
+                              className="2xl:w-[70px] 2xl:h-[70px] w-[60px] h-[60px]"
                               alt=""
                             />
 
                             <div className="flex flex-col gap-2">
-                              <p className="text-blue-500 font-bold">
+                              <p className="text-blue-500 2xl:text-base text-sm font-bold">
                                 {i?.name}
                               </p>
-                              <p className="text-sm text-gray-500">{i?.uid}</p>
+                              <p className="2xl:text-sm text-xs text-gray-500">
+                                {i?.uid}
+                              </p>
                             </div>
                           </div>
                           <div className="flex gap-5 items-center">
-                            <p className="text-green-500 font-bold">
+                            <p className="2xl:text-base text-sm text-green-500 font-bold">
                               {i?.taskNum} accomplished tasks
                             </p>
-                            <p className="text-blue-500 text-2xl font-bold">
+                            <p className="2xl:text-2xl text-xl text-blue-500 font-bold">
                               {i?.points}
                             </p>
                           </div>
@@ -339,27 +347,29 @@ function Leaderboards() {
                         <img
                           src="https://cdn3d.iconscout.com/3d/premium/thumb/silver-medal-8616546-6815635.png"
                           alt=""
-                          className="h-[70px]"
+                          className="2xl:h-[70px] h-[50px]"
                         />
-                        <div className="rounded-full bg-blue-100 w-2/5 flex items-center justify-between pr-10">
+                        <div className="rounded-full bg-blue-100 w-[485px] flex items-center justify-between pr-10">
                           <div className="flex gap-5 items-center">
                             <img
                               src={i?.profileAvatar}
-                              className="w-[70px] h-[70px]"
+                              className="2xl:w-[70px] 2xl:h-[70px] w-[60px] h-[60px]"
                               alt=""
                             />
                             <div className="flex flex-col gap-2">
-                              <p className="text-blue-500 font-bold">
+                              <p className=" 2xl:text-base text-sm text-blue-500 font-bold">
                                 {i?.name}
                               </p>
-                              <p className="text-sm text-gray-500"> {i?.uid}</p>
+                              <p className="2xl:text-sm text-xs text-gray-500">
+                                {i?.uid}
+                              </p>
                             </div>
                           </div>
                           <div className="flex gap-5 items-center">
-                            <p className="text-green-500 font-bold">
+                            <p className="2xl:text-base text-sm text-green-500 font-bold">
                               {i?.taskNum} accomplished tasks
                             </p>
-                            <p className="text-blue-500 text-2xl font-bold">
+                            <p className="2xl:text-2xl text-xl text-blue-500 font-bold">
                               {i?.points}
                             </p>
                           </div>
@@ -372,27 +382,29 @@ function Leaderboards() {
                         <img
                           src="https://cdn3d.iconscout.com/3d/premium/thumb/bronze-medal-8616543-6815632.png"
                           alt=""
-                          className="h-[70px]"
+                          className="2xl:h-[70px] h-[50px]"
                         />
-                        <div className="rounded-full bg-blue-100 w-2/5 flex items-center justify-between pr-10">
+                        <div className="rounded-full bg-blue-100 w-[485px] flex items-center justify-between pr-10">
                           <div className="flex gap-5 items-center">
                             <img
                               src={i?.profileAvatar}
-                              className="w-[70px] h-[70px]"
+                              className="2xl:w-[70px] 2xl:h-[70px] w-[60px] h-[60px]"
                               alt=""
                             />
                             <div className="flex flex-col gap-2">
-                              <p className="text-blue-500 font-bold">
+                              <p className="2xl:text-base text-sm text-blue-500 font-bold">
                                 {i?.name}
                               </p>
-                              <p className="text-sm text-gray-500"> {i?.uid}</p>
+                              <p className="2xl:text-sm text-xs text-gray-500">
+                                {i?.uid}
+                              </p>
                             </div>
                           </div>
                           <div className="flex gap-5 items-center">
-                            <p className="text-green-500 font-bold">
+                            <p className="2xl:text-base text-sm text-green-500 font-bold">
                               {i?.taskNum} accomplished tasks
                             </p>
-                            <p className="text-blue-500 text-2xl font-bold">
+                            <p className="2xl:text-2xl text-xl text-blue-500 font-bold">
                               {i?.points}
                             </p>
                           </div>
@@ -405,28 +417,30 @@ function Leaderboards() {
                         className="flex justify-center gap-10 items-center"
                         key={z}
                       >
-                        <p className="text-blue-500 font-bold text-2xl w-[70px] flex justify-center">
+                        <p className="text-blue-500 font-bold text-2xl 2xl:w-[70px] w-[50px] flex justify-center">
                           {z + 1}
                         </p>
-                        <div className="rounded-full bg-blue-100 w-2/5 flex items-center justify-between pr-10">
+                        <div className="rounded-full bg-blue-100 w-[485px] flex items-center justify-between pr-10">
                           <div className="flex gap-5 items-center">
                             <img
                               src={i?.profileAvatar}
-                              className="w-[70px] h-[70px]"
+                              className="2xl:w-[70px] 2xl:h-[70px] w-[60px] h-[60px]"
                               alt=""
                             />
                             <div className="flex flex-col gap-2">
-                              <p className="text-blue-500 font-bold">
+                              <p className="2xl:text-base text-sm text-blue-500 font-bold">
                                 {i?.name}
                               </p>
-                              <p className="text-sm text-gray-500"> {i?.uid}</p>
+                              <p className="2xl:text-sm text-xs text-gray-500">
+                                {i?.uid}
+                              </p>
                             </div>
                           </div>
                           <div className="flex gap-5 items-center">
-                            <p className="text-green-500 font-bold">
+                            <p className="2xl:text-base text-sm text-green-500 font-bold">
                               {i?.taskNum} accomplished tasks
                             </p>
-                            <p className="text-blue-500 text-2xl font-bold">
+                            <p className="2xl:text-2xl text-xl text-blue-500 font-bold">
                               {i?.points}
                             </p>
                           </div>
